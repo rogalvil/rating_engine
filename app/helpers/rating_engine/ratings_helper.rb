0f3eeaa324a5027382b73245a87187a427552883
e3matheus <e3matheus@gmail.com>
2012-05-01 20:12:17 -0500

    Return an empty url if no user is signed in

--- app/helpers/rating_engine/ratings_helper.rb
+++ app/helpers/rating_engine/ratings_helper.rb
@@ -1,7 +1,8 @@
 module RatingEngine
   module RatingsHelper
     def star_rating_for(rateable, params={})
-      enabled, show_score = params[:enabled].nil? ? true : params[:enabled], (params[:show_score] || true)
+      enabled = params[:enabled].nil? ? true : params[:enabled]
+      show_score = params[:show_score] || true
       current_score = show_score ? score_in_pixels(rateable.average_rating) : 0
     
       render :partial => 'rating_engine/ratings/star_rating', 
@@ -14,7 +15,9 @@ module RatingEngine
     end
   
     def rating_url(rateable)
-      if rateable.rated_by?(controller.current_user)
+      if current_user.nil?
+        ""
+      elsif rateable.rated_by?(controller.current_user)
         eval("#{rateable.class.to_s.downcase}_rating_path(#{rateable.id})")
       else
         eval("#{rateable.class.to_s.downcase}_ratings_path(#{rateable.id})")
